6161a37ec38a526c692153220ec8b6450f2d2e6e
user <user@es302n03>
2018-09-25 11:58:55 +0800

    initial

---
+++ my project/src/com/tom/hello.java
@@ -0,0 +1,13 @@
+package com.tom;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ float num =(float)52.5;
+ 
+ 
+ System.out.println(num);
+	}
+
+}
