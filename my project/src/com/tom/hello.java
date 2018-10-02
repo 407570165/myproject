ecaa92c24f219c9c9e03d79b801adaa75eb3bb6f
justin <user@es302n02>
2018-10-02 10:41:08 +0800

    second

--- my project/src/com/tom/hello.java
+++ my project/src/com/tom/hello.java
@@ -4,10 +4,14 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- float num =(float)52.5;
+ int age=20; 
+ float weigh=63.5f;
+ float heigh=174f;
+ System.out.println("age"+age);
+ System.out.println("weigh"+weigh);
+ System.out.println("heigh"+heigh);
  
  
- System.out.println(num);
 	}
 
 }
